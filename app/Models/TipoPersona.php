035aa7889d4de19bd96d935e654ba07318ee0d22
Andres Orozco Muñoz <andres.orozco@ceiba.com.co>
2020-08-16 17:58:18 -0500

    Se ajusta el modelo de persona y se adicion model de tipo persona

---
+++ app/Models/TipoPersona.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TipoPersona extends Model
+{
+    public $table = 'tbl_tipo_de_persona';
+    protected $fillable = ['nombre'];
+    public $timestamps = false;
+}
