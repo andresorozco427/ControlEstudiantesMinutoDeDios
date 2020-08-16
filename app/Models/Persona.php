075baf9ac1f28e2035f74424f665a696000398f4
joseusuga <jose.usuga@ceiba.com.co>
2020-08-16 18:08:37 -0500

    Formatear código

--- app/Models/Persona.php
+++ app/Models/Persona.php
@@ -7,8 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Persona extends Model
 {
     public $table = 'tbl_persona';
-    protected $fillable = ['identificacion', 'id_tipo_identificacion', 'nombre', 'apellido', 'id_sexo','direccion',
-'telefono', 'correo', 'profesion', 'id_departamento','id_municipio', 'id_tipo_de_persona', 'edad'];
+    protected $fillable = [
+        'identificacion', 'id_tipo_identificacion', 'nombre', 'apellido', 'id_sexo', 'direccion',
+        'telefono', 'correo', 'profesion', 'id_departamento', 'id_municipio', 'id_tipo_de_persona', 'edad'
+    ];
     public $timestamps = false;
     protected $primaryKey = 'identificacion';
     protected $keyType = 'string';
